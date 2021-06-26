639e632733cbc2502dd373fac3e84daa25d877f9
Mike <2005jayceh@gmail.com>
2021-06-26 15:33:30 -0400

    Still Writing

--- src/server/RayHandler.server.lua
+++ src/server/RayHandler.server.lua
@@ -1,32 +1,36 @@
 -- Written by Sqowly for no reason @ may/15/21 12-1 AM.
--- Edited By Soqwly also for no reason @ june/25/2021 1:30-2:00 AM.
+-- Edited By Sqowly also for no reason @ june/25/2021 1:30-2:00 AM.
 local SpawnLocation : any = workspace.SpawnLocation
 local isFinishedRaying : boolean  = false
-local maxNum : number = 5
-local self = {
-	raysize = 50;
-}
 local collidable : any = workspace:GetChildren()
+local maxNum : number = 5
+
+local params = {raysize = 50}
 local collisionObjects = {}
 local global_direction
 local all = {}
+
+function ApplyHealthToAllObjects(x : any)
+	x:SetAttribute("minHealth", 50)
+	x:SetAttribute("maxHealth", 100)
+end
 
 function MakeRay()
 	table.insert(collidable)
 	while wait(0.5) do
 		for i in pairs (collisionObjects) do
-			if collisionObjects[i][#collidable]:IsA "Part" then
+			if (collisionObjects[i][#collidable]:IsA "Part") then
 				local objects : any  = collisionObjects[i][#collidable] -- get position from this
 				local origin : Vector3 = Vector3.new(0, math.floor(SpawnLocation.Position.Y), 0)
-				local direction : Vector3 = Vector3.new(0, origin * self.raysize, 0)
+				local direction : Vector3 = Vector3.new(0, origin * params.raysize, 0)
 				local newRay : any = Ray.new(origin, -direction/objects.position.Y)
 				print(newRay)
 				while wait() do
 					local part : any = Instance.new("Part", workspace)
 					part.Anchored = true
-					if part ~= nil then
+					if (part ~= nil) then
 						print(newRay.Origin.Y) -- when dir = 0
-						part.Size = Vector3.new(0, self.raysize, 0)
+						part.Size = Vector3.new(0, params.raysize, 0)
 						part.Size += Vector3.new(0, -newRay.Direction, 0)
 						game:GetService("Debris"):AddItem(part, 1)
 						for count : number = 1, 5 do
@@ -35,7 +39,7 @@ function MakeRay()
 								for timer : number = 1, maxNum do
 									if (timer == maxNum) then
 										isFinishedRaying = true
-										if isFinishedRaying then
+										if (isFinishedRaying) then
 											global_direction  = {}
 											table.insert(global_direction, newRay)
 											for dir in pairs (global_direction) do
@@ -46,6 +50,7 @@ function MakeRay()
 										local allObjects : any = collisionObjects[i][#collidable]
 										table.insert(all, allObjects)
 										for ofu in pairs (all) do
+											ApplyHealthToAllObjects(all[ofu])
 											wait(count + timer)
 											table.remove(global_direction, num)
 											AlteredRayDirection = -RayDir/all[ofu].Position.Y
@@ -60,7 +65,7 @@ function MakeRay()
 					end
 				end
 			else
-				return collisionObjects[i]
+				return collisionObjects[i][#collidable]
 			end
 		end
 	end
