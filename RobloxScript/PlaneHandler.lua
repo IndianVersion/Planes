fba0d349056809135dd7625c6b502f9d51b4f3ab
Mike <2005jayceh@gmail.com>
2021-07-02 13:55:01 -0400

    Adding

--- RobloxScript/PlaneHandler.lua
+++ RobloxScript/PlaneHandler.lua
@@ -1,5 +1,10 @@
-local PlaneHandler = {}
-local UserInputService = game:GetService("UserInputService")
+local PlaneHandler = {};
+local UserInputService = game:GetService("UserInputService");
+local PlaneCenter : any = workspace.PlaneCenter;
+local taxiing : boolean = false;
+local velSet : boolean = false;
+local afterVelSet : boolean = false;
+local numsCounter : any = {};
 
 local controllingKeys : any = {
 	forward == "W";
@@ -13,12 +18,31 @@ function PlaneHandler.AllKeysToFly(touchingGround : boolean)
 		UserInputService.InputBegan:Connect(
 			function (input : any, isTyping : boolean)
 				if input.KeyCode == Enum.KeyCode[controllingKeys[int]] then
-					flying = true
-					local planeTaxiingSpeed : number = 30
-					local taxiingDefault : number = 0
+					flying = true;
+					planeTaxiingSpeed = 30;
+					taxiingDefault = 0;
 				end
 				if input.KeyCode == Enum.KeyCode[controllingKeys.forward] then
 					-- apply force to -z
+					taxiing = true;
+					if taxiing then
+						while wait(0.1) do
+							taxiingDefault += 1;
+							if taxiingDefault == planeTaxiingSpeed then
+								velSet = true;
+								if velSet then
+									table.insert(numsCounter, taxiingDefault)
+									afterVelSet = true;
+									break
+								end
+							end
+						end
+						if afterVelSet then
+							for x in pairs (numsCounter) do
+								
+							end
+						end
+					end
 				end
 			end
 		)
@@ -30,4 +54,4 @@ function PlaneHandler.RemapKeys()
 end
 
 
-return PlaneHandler
+return PlaneHandler;
