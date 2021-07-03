36e8ec07263426a0b1850ed63ff6a81ef6e239c5
Mike <2005jayceh@gmail.com>
2021-07-03 01:04:58 -0400

    Coding

--- RobloxScript/GetDirection.lua
+++ RobloxScript/GetDirection.lua
@@ -1,66 +1,66 @@
-local RunService = game:GetService("RunService");
-local AirplaneNose : any = workspace.AirplaneNose;
-local PlaneCenter : any = workspace.PlaneCenter;
-local stalled : boolean = false;
-local touchingGround : boolean = false;
-local stallHeight : number = 500;
-local ImageLabel : any = script.Parent;
-local MaxRot : number = 180;
-local storeAirplaneNosePos : any = {};
-local storePlanePos : any = {};
-local flying : boolean = _G.flying;
-local PlaneHandler : any = require(game.ReplicatedStorage.Common:WaitForChild("PlaneHandler"));
+local RunService = game:GetService("RunService")
+local AirplaneNose : any = workspace.AirplaneNose
+local PlaneCenter : any = workspace.PlaneCenter
+local stalled : boolean = false
+local touchingGround : boolean = false
+local stallHeight : number = 500
+local ImageLabel : any = script.Parent
+local MaxRot : number = 180
+local storeAirplaneNosePos : any = {}
+local storePlanePos : any = {}
+local flying : boolean = _G.flying
+local PlaneHandler : any = require(game.ReplicatedStorage.Common:WaitForChild("PlaneHandler"))
 
 function TriggerResetX(x : number)
-	return x == 0;
+	return x == 0
 end
 
 function TriggerOnStall()
-	
+
 end
 
 RunService.RenderStepped:Connect(function()
-	table.insert(storeAirplaneNosePos, AirplaneNose.Position);
-	table.insert(storePlanePos, PlaneCenter.Position);
+	table.insert(storeAirplaneNosePos, AirplaneNose.Position)
+	table.insert(storePlanePos, PlaneCenter.Position)
 	for i in pairs (storeAirplaneNosePos) do
-		all = i;
+		all = i
 		if storeAirplaneNosePos[i] ~= nil then
 			if storeAirplaneNosePos[i].X ~= 0 then --only clamped x y will be inf since 360 is a thing
-				local clampedX : number = math.clamp(storeAirplaneNosePos[i].X, 90, 180);
-				ImageLabel.Position = Vector2.new(clampedX, 0);
+				local clampedX : number = math.clamp(storeAirplaneNosePos[i].X, 90, 180)
+				ImageLabel.Position = Vector2.new(clampedX,0)
 				if storeAirplaneNosePos[1].X ~= nil then
 					if ImageLabel.Position.X == MaxRot then
-						TriggerResetX(storeAirplaneNosePos[i].X);
+						TriggerResetX(storeAirplaneNosePos[i].X)
 					end
 				end
 			else
-				return storeAirplaneNosePos[i].X;
+				return storeAirplaneNosePos[i].X
 			end
 			if storeAirplaneNosePos[i].Y < 90 then
-				ImageLabel.Rotation = storeAirplaneNosePos[i].Y;
+				ImageLabel.Rotation = storeAirplaneNosePos[i].Y
 				for o in pairs (storePlanePos) do
-					nums = o;
-					local origin : Vector3 = Vector3.new(0, -PlaneCenter.Position.Y, 0);
-					local direction : Vector3 = Vector3.new(0, math.huge, 0);
-					local cast : any = Ray.new(origin, direction);
-					local DirBeam : any = Instance.new("Part", workspace);
-					DirBeam.Transparency = 1;
-					DirBeam.Anchored = true;
-					DirBeam.CanCollide = false;
-					DirBeam.Color = Color3.fromRGB(255, 0, 0);
-					local ScaleDist : number = (storePlanePos[o] - workspace.Baseplate.Position).magnitude;
-					DirBeam.Size = Vector3.new(2.5, -cast.Direction/storePlanePos[o].Y+ScaleDist, 2.5);
-					local RayAndBaseplate : any = table.pack(workspace.Baseplate, DirBeam);
+					nums = o
+					local origin : Vector3 = Vector3.new(0,-PlaneCenter.Position.Y,0)
+					local direction : Vector3 = Vector3.new(0,math.huge,0)
+					local cast : any = Ray.new(origin, direction)
+					local DirBeam : any = Instance.new("Part", workspace)
+					DirBeam.Transparency = 1
+					DirBeam.Anchored = true
+					DirBeam.CanCollide = false
+					DirBeam.Color = Color3.fromRGB(255,0,0)
+					local ScaleDist : number = (storePlanePos[o] - workspace.Baseplate.Position).magnitude
+					DirBeam.Size = Vector3.new(2.5,-cast.Direction/storePlanePos[o].Y+ScaleDist,2.5)
+					local RayAndBaseplate : any = table.pack(workspace.Baseplate, DirBeam)
 					if workspace:ArePartsTouchingOthers(RayAndBaseplate) == nil then
-						touchingGround = true;
+						touchingGround = true
 						while touchingGround do
 							if flying then
-								PlaneHandler.AllKeysToFly(touchingGround);
+								PlaneHandler.AllKeysToFly(touchingGround)
 								break
 							end
 						end
 					else
-						touchingGround = false;
+						touchingGround = false
 					end
 				end
 			end
@@ -69,9 +69,9 @@ RunService.RenderStepped:Connect(function()
 		end
 	end
 	for liteDel : number = 1, all do
-		table.remove(storeAirplaneNosePos, liteDel);
+		table.remove(storeAirplaneNosePos, liteDel)
 	end
 	for liteDel_2 : number = 1, nums do
-		table.remove(storePlanePos, liteDel_2);
+		table.remove(storePlanePos, liteDel_2)
 	end
 end)
--- RobloxScript/PlaneHandler.lua
+++ RobloxScript/PlaneHandler.lua
@@ -1,10 +1,13 @@
-local PlaneHandler = {};
-local UserInputService = game:GetService("UserInputService");
-local PlaneCenter : any = workspace.PlaneCenter;
-local taxiing : boolean = false;
-local velSet : boolean = false;
-local afterVelSet : boolean = false;
-local numsCounter : any = {};
+local PlaneHandler = {}
+
+local UserInputService = game:GetService("UserInputService")
+local PlaneCenter : any = workspace.PlaneCenter
+local taxiing : boolean = false
+local velSet : boolean = false
+local afterVelSet : boolean = false
+local fullStop : boolean = false
+local numsCounter : any = {}
+local numForMinus : any = {}
 
 local controllingKeys : any = {
 	forward == "W";
@@ -15,31 +18,35 @@ local controllingKeys : any = {
 
 function PlaneHandler.AllKeysToFly(touchingGround : boolean)
 	for int in pairs (controllingKeys) do
+		every = int
 		UserInputService.InputBegan:Connect(
 			function (input : any, isTyping : boolean)
 				if input.KeyCode == Enum.KeyCode[controllingKeys[int]] then
-					flying = true;
-					planeTaxiingSpeed = 30;
-					taxiingDefault = 0;
+					flying = true
+					planeTaxiingSpeed = 30
+					taxiingDefault = 0
 				end
 				if input.KeyCode == Enum.KeyCode[controllingKeys.forward] then
 					-- apply force to -z
-					taxiing = true;
+					taxiing = true; -- use for triggering a stop on the plane
 					if taxiing then
 						while wait(0.1) do
-							taxiingDefault += 1;
+							taxiingDefault += 1
 							if taxiingDefault == planeTaxiingSpeed then
-								velSet = true;
+								velSet = true
 								if velSet then
 									table.insert(numsCounter, taxiingDefault)
-									afterVelSet = true;
+									afterVelSet = true
 									break
 								end
 							end
 						end
 						if afterVelSet then
 							for x in pairs (numsCounter) do
-								
+								bodyvelocity = Instance.new("BodyVelocity", PlaneCenter)
+								bodyvelocity.Velocity = Vector3.new(0,0,-PlaneCenter.Position.Z*numsCounter[x])
+								bodyvelocity.P = -taxiingDefault*2
+								bodyvelocity.MaxForce = Vector3.new(0,0,-numsCounter[x]*5)
 							end
 						end
 					end
@@ -49,9 +56,34 @@ function PlaneHandler.AllKeysToFly(touchingGround : boolean)
 	end
 end
 
+function PlaneHandler.AllKeysToStop()
+	UserInputService.InputEnded:Connect(
+		function (input : any, isTyping : boolean)
+			if isTyping then return end
+			if input.KeyCode == Enum.KeyCode[controllingKeys[every]] then
+				flying = false
+				while not flying do
+					wait(0.1)
+					taxiingDefault -= 1
+					table.insert(numForMinus, taxiingDefault)
+					if taxiingDefault == 0 then
+						fullStop = true
+					end
+				end
+				if fullStop then
+					for minus in pairs (numForMinus) do
+						local cancel = numForMinus[minus]
+						return bodyvelocity.Velocity == Vector3.new(cancel,cancel,cancel)
+					end
+				end
+			end
+		end
+	)
+end
+
 function PlaneHandler.RemapKeys()
 	
 end
 
 
-return PlaneHandler;
+return PlaneHandler
